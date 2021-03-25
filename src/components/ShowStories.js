42120ccac299949c08b58d2fbdcbce5aaf91778b
Ryan Rotman <ryan.rotman@gmail.com>
2021-03-25 15:08:34 -0500

    edited for Loader component implimentation

--- src/components/ShowStories.js
+++ src/components/ShowStories.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Story from './Story';
 import useDataFetcher from '../hooks/dataFetcher';
+import Loader from './Loader';
 
-const ShowStories = ({ type }) => {
-    const { isLoading, stories } = useDataFetcher(type ? type : 'top');
+const ShowStories = (props) => {
+    const { type } = props.match.params;
+    const { isLoading, stories } = useDataFetcher(type);
 
     return (
         <React.Fragment>
-            {isLoading ? (
-            <p className="loading">Loading...</p>
-            ) : (
+            <Loader show={isLoading}>Loading...</Loader>
             <React.Fragment>
                 {stories.map(({ data: story }) => (
                 <Story key={story.id} story={story} />
                 ))}
             </React.Fragment>
-            )}
         </React.Fragment>
     );
 };
