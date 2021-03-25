30dfc8bcc3de13d347d307b7d8604f61523e5ce3
Ryan Rotman <ryan.rotman@gmail.com>
2021-03-25 15:14:55 -0500

    reverted top part of code to take care of error

--- src/components/ShowStories.js
+++ src/components/ShowStories.js
@@ -3,18 +3,17 @@ import Story from './Story';
 import useDataFetcher from '../hooks/dataFetcher';
 import Loader from './Loader';
 
-const ShowStories = (props) => {
-    const { type } = props.match.params;
-    const { isLoading, stories } = useDataFetcher(type);
+const ShowStories = ({ type }) => {
+    const { isLoading, stories } = useDataFetcher(type ? type : 'top');
 
     return (
         <React.Fragment>
-            <Loader show={isLoading}>Loading...</Loader>
-            <React.Fragment>
-                {stories.map(({ data: story }) => (
-                <Story key={story.id} story={story} />
-                ))}
-            </React.Fragment>
+        <Loader show={isLoading}>Loading...</Loader>
+        <React.Fragment>
+            {stories.map(({ data: story }) => (
+            <Story key={story.id} story={story} />
+            ))}
+        </React.Fragment>
         </React.Fragment>
     );
 };
