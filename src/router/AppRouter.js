61a991916ec45e895fc849b7c86f29110d098562
Ryan Rotman <ryan.rotman@gmail.com>
2021-03-25 14:45:45 -0500

    added new route and corresponding import

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import HomePage from '../components/HomePage';
+import ShowStories from '../components/ShowStories';
 import PageNotFound from '../components/PageNotFound';
 
 const AppRouter = () => {
@@ -11,6 +12,7 @@ const AppRouter = () => {
             <Header />
             <Switch>
                 <Route path="/" component={HomePage} exact={true} />
+                <Route path="/:type" component={ShowStories} />
                 <Route component={PageNotFound} />
             </Switch>
             </div>
